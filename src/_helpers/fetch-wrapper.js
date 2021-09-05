21b2be8d174aa938ff9d934b620cb03f5f4b7513
Jason Watmore <cornflourblue@hotmail.com>
2021-09-05 10:23:27 +1000

    refactored fetch wrapper

--- src/_helpers/fetch-wrapper.js
+++ src/_helpers/fetch-wrapper.js
@@ -9,46 +9,24 @@ function useFetchWrapper() {
     const [auth, setAuth] = useRecoilState(authAtom);
 
     return {
-        get,
-        post,
-        put,
-        delete: _delete
+        get: request('GET'),
+        post: request('POST'),
+        put: request('PUT'),
+        delete: request('DELETE')
     };
 
-    function get(url) {
-        const requestOptions = {
-            method: 'GET',
-            headers: authHeader(url)
-        };
-        return fetch(url, requestOptions).then(handleResponse);
-    }
-    
-    function post(url, body) {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', ...authHeader(url) },
-            credentials: 'include',
-            body: JSON.stringify(body)
-        };
-        return fetch(url, requestOptions).then(handleResponse);
-    }
-    
-    function put(url, body) {
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json', ...authHeader(url) },
-            body: JSON.stringify(body)
-        };
-        return fetch(url, requestOptions).then(handleResponse);    
-    }
-    
-    // prefixed with underscored because delete is a reserved word in javascript
-    function _delete(url) {
-        const requestOptions = {
-            method: 'DELETE',
-            headers: authHeader(url)
-        };
-        return fetch(url, requestOptions).then(handleResponse);
+    function request(method) {
+        return (url, body) => {
+            const requestOptions = {
+                method,
+                headers: authHeader(url)
+            };
+            if (body) {
+                requestOptions.headers['Content-Type'] = 'application/json';
+                requestOptions.body = JSON.stringify(body);
+            }
+            return fetch(url, requestOptions).then(handleResponse);
+        }
     }
     
     // helper functions
